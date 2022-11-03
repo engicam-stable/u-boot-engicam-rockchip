02da4e927ae7163e6619989b3cdb6134f6565467
matteopasqualetti <matteo.pasqualetti@engicam.com>
2022-11-03 12:17:47 +0100

    Added control on EMMC_nEN gpio

--- arch/arm/mach-rockchip/px30/px30.c
+++ arch/arm/mach-rockchip/px30/px30.c
@@ -17,6 +17,18 @@
 #include <asm/arch/cru_px30.h>
 #include <dt-bindings/clock/px30-cru.h>
 
+#include <misc.h>
+#include <usb.h>
+#include <dwc3-uboot.h>
+#include <spl.h>
+#include <u-boot/sha256.h>
+#include <power/regulator.h>
+#include <dm/pinctrl.h>
+#include <dm/uclass-internal.h>
+#include <asm/gpio.h>
+#include <asm/setup.h>
+#include <asm/arch/periph.h>
+
 #define PMU_PWRDN_CON			0xff000018
 #define GRF_CPU_CON1			0xff140504
 
@@ -799,18 +811,37 @@ int rk_board_early_fdt_fixup(const void *blob)
 	return 0;
 }
 
+DECLARE_GLOBAL_DATA_PTR;
+#define PMUGRF_GPIO0B_IOMUX_ADDR		0xFF010004
+#define GRF_IO_VSEL_ADDR                0xFF140180
+#define EMMC_nEN_GPIO (8 + 6) /* GPIO0 RK_PB6 */
+
 #if !defined(CONFIG_SPL_BUILD) && defined(CONFIG_ROCKCHIP_DMC_FSP)
 int rk_board_init(void)
 {
 	struct udevice *dev;
 	u32 ret = 0;
 
+	unsigned long *pmugrf;
+    unsigned long *grf_vsel;
+    unsigned int sys_reg;
+
+	pmugrf = (void *)PMUGRF_GPIO0B_IOMUX_ADDR;
+    sys_reg = 0x10000000;
+
+    writel(sys_reg, pmugrf);
+    grf_vsel = (void *)GRF_IO_VSEL_ADDR;
+    sys_reg = 0x00030003;
+
+    writel(sys_reg, grf_vsel);
+    gpio_request(EMMC_nEN_GPIO, "emmc enabled");
+    gpio_direction_output(EMMC_nEN_GPIO, 0);
+
 	ret = uclass_get_device_by_driver(UCLASS_DMC, DM_GET_DRIVER(dmc_fsp), &dev);
 	if (ret) {
 		printf("dmc_fsp failed, ret=%d\n", ret);
 		return 0;
 	}
-
 	return 0;
 }
 #endif
